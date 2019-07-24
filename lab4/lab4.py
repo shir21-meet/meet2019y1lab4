475a41909e00e44cb7b03ae470005f2483a12c52
shir21-meet <shir21@meet.mit.edu>
2019-07-24 05:36:11 -0400

    Stopping here

---
+++ lab4/lab4.py
@@ -0,0 +1,4 @@
+tab = False
+if tab:
+    print('chocolate')
+print('Tabs are cool!')
---
+++ lab4/zdrgsd.py
@@ -0,0 +1,13 @@
+quastions="what is the fruit"
+print(quastions)
+answer=input(">")
+if answer == "apple":
+    print("apple in bin1")
+elif answer == "orenge":
+    print("orenge in bin2")
+elif answer == "banna":
+    print("banna in bin3")
+else:
+    print("eror")
+
+
